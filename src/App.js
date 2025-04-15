2842ddbf57ee5eb55617c8c29537a2e02a1c2a03
Soumya Ghosh <soumyaghosh825@gmail.com>
2025-04-15 17:06:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "6609e664db7d259df6bae6a400cd218e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
