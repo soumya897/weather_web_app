41c0d15490f6e29242e2be2cde304ca3c95c4bf5
Soumya Ghosh <soumyaghosh825@gmail.com>
2025-04-16 20:33:22 +0530

    build folder committed

---
+++ build/precache-manifest.5d969d0822bcf6e3c4fabc685cda494b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836ab9475befc87b85d981cef4c5068f",
+    "url": "/weather_web_app/index.html"
+  },
+  {
+    "revision": "b36f7848b25dc408f1ab",
+    "url": "/weather_web_app/static/css/main.cbb3691a.chunk.css"
+  },
+  {
+    "revision": "6dcfc77f0f18ba424a25",
+    "url": "/weather_web_app/static/js/2.701fe897.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather_web_app/static/js/2.701fe897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b36f7848b25dc408f1ab",
+    "url": "/weather_web_app/static/js/main.1160c3b0.chunk.js"
+  },
+  {
+    "revision": "1db6f233550f3b0c989a",
+    "url": "/weather_web_app/static/js/runtime-main.8db31719.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather_web_app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/weather_web_app/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/weather_web_app/static/media/city.e1698332.jpg"
+  }
+]);
